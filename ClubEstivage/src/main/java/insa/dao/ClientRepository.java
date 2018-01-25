ab99088a7145aeeb269fafd05e623d56f08c7253
SAFAEATLA <elatlasafae@gmail.com>
2018-01-25 21:41:54 +0100

    client front end

--- ClubEstivage/src/main/java/insa/dao/ClientRepository.java
+++ ClubEstivage/src/main/java/insa/dao/ClientRepository.java
@@ -1,10 +1,15 @@
 package insa.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import insa.entities.Client;
 
 public interface ClientRepository extends JpaRepository<Client,Long>  {
+	@Query("select c from Client c where c.nomClient like:x")
+	public Page<Client> chercher(@Param("x")String mc, Pageable pageable);
 
 	
 	
--- ClubEstivage/src/main/java/insa/web/ClientRestService.java
+++ ClubEstivage/src/main/java/insa/web/ClientRestService.java
@@ -3,14 +3,19 @@ package insa.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import insa.dao.ClientRepository;
 import insa.entities.Client;
+import insa.entities.User;
 
 @RestController
 @CrossOrigin("*")
@@ -32,7 +37,26 @@ public class ClientRestService {
 		
 	}
 	
+	@RequestMapping(value="/chercherClients",method=RequestMethod.GET)
+	public Page<Client> chercher(
+			@RequestParam (name="mc", defaultValue="") String mc,
+			@RequestParam (name="page", defaultValue="0") int page,
+			@RequestParam (name="size", defaultValue="5") int size
+			){
+		return clientRepository.chercher("%"+mc+"%", new PageRequest(page, size));
+		
+	}
 	
-	
+	@RequestMapping(value="/clients",method=RequestMethod.POST)
+	public Client save(@RequestBody Client c){
+		return clientRepository.save(c);
+		
+	}
+	@RequestMapping(value="/clients/{id}",method=RequestMethod.DELETE)
+	public boolean  supprimer(@PathVariable Long id){
+		clientRepository.delete(id);
+	    return true;
+		 
+	}
 	
 }
